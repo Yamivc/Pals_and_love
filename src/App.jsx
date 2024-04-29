926be5b08692d9b12d799f79b96ffdaf3051f2a7
Yamiley H.G <yamihego99@gmail.com>
2024-04-29 14:41:28 +0200

    fixing the button comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Home from './components/pages/Home'
 import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-    
+    <Router>
+    <Route path='/' exact component={Home} />
+
+    </Router>
     </>
   )
 }
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../../App.css';
+import Footer from '../footer/Footer';
+
+function Home() {
+    return (
+        <>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
