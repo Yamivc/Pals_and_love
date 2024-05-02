0ed89f26ebab0f4591b125715adc48061f19bcf4
Yamiley H.G <yamihego99@gmail.com>
2024-05-02 13:18:16 +0200

    ArrayImage implemented and responsive

--- src/components/array-images/ArrayImage.jsx
+++ src/components/array-images/ArrayImage.jsx
@@ -1,24 +1,29 @@
 import imageArray from "../../models/image-array-image-and-text/imageArrayImageAndText";
 import "./ArrayImage.css";
+import ImageSpinner from "../image-spinner/ImageSpinner";
+import { Link } from 'react-router-dom';
+
+
 
 function ImageArrayImageAndText() {
   return (
     <>
       <div className="image-array-image-and-text-wrapper">
-        <div className="image-array-image-and-text-little-title">La ruta del agua de Telde</div>
-        <div className="image-array-image-and-text-big-title">un proyecto, dos centros, tres logos...</div>
-        <div className="image-array-image-and-text-tiny-title">un proyecto, dos centros, tres logos, dos profesores y mucho alumnado.</div>
+        <div className="image-array-image-and-text-little-title"><h1>Information To The Power</h1></div>
         <div className="image-array-image-and-text-images-wrapper">
           {
             imageArray.map((image, index) => (
               <div className="image-array-image-and-text-unit" key={index}>
                 <div className="image-array-image-and-text-unit-image-wrapper">
-                  <img src={`/images/route/${image.src}.jpg`} alt="array image" />
+                  <ImageSpinner imageUrl={`src/images/${image.src}.jpg`} alt="array image" />
                 </div>
                 <div className="image-array-image-and-text-unit-text">
                   <h2>{image.title}</h2>
                   <p>{image.description}</p>
-                  <button>Leer más...</button>
+                  <Link to="/learn-more">
+                    <button>Read More...</button>
+                  </Link>
+
                 </div>
               </div>
             ))
--- src/models/image-array-image-and-text/imageArrayImageAndText.js
+++ src/models/image-array-image-and-text/imageArrayImageAndText.js
@@ -1,28 +1,24 @@
 let imageArrayAndText = [
   {
-    src: "20240126_084119",
-    title: "Presentación",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
+    src: "image8",
+    title: "Anxiety and stress, Modern lifes",
+    description: "Stress and anxiety are like uninvited guests that always seem to find a way into my life, no matter how hard I try to keep them out. When they arrive, they bring with them a heavy..."
   },{
-    src: "20240126_084249",
-    title: "Rúbrica",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
+    src: "image6",
+    title: "Tech-addicted",
+    description: "Tech addiction is a complex challenge that I find myself grappling with daily. It's a constant battle to resist the allure of my devices, each ping and notification tempting me to dive back into the digital world..."
   },{
-    src: "20240126_115015",
-    title: "Naturaleza",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
+    src: "image5",
+    title: "Depression, the silence death",
+    description: "Depression is like sinking into a dark abyss where everything feels heavy and hopeless. It's like carrying around a weight that no one else can see, dragging me down with every step I take. It's waking up each morning with a sense of dread, knowing that another day of emptiness awaits... "
   },{
-    src: "20240126_113628",
-    title: "Historia",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
+    src: "image7",
+    title: "Burnout and how to confront it",
+    description: "Burnout is a state of emotional, physical, and mental exhaustion caused by prolonged stress, often related to work or caregiving responsibilities. It is characterized by feelings of fatigue, cynicism, and a sense of ineffectiveness or lack of accomplishment..."
   },{
-    src: "20240126_120356",
-    title: "Convivencia",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
-  },{
-    src: "20240126_120531",
-    title: "Raíces de un pueblo",
-    description: "Una descripción de la imagen que sea de dos líneas o así."
+    src: "image9",
+    title: "Whats is Mental Health?",
+    description: "Mental health encompasses our emotional, psychological, and social well-being. It influences how we think, feel, and act, and it also determines how we handle stress, relate to others, and make choices. Mental health is vital at every stage of life, from childhood and adolescence through adulthood..."
   }
 ];
 
